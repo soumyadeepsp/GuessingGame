d453a5870cecb2c3975826ef4417ff6cc6034af6
soumyadeepsp <soumyadeepsp@gmail.com>
2023-08-09 22:05:27 +0530

    started project

---
+++ components/PrimaryButton.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function PrimaryButton() {
+  return (
+    <View>
+      <TextInput/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function StartGameScreen() {
+  return (
+    <View>
+      <TextInput/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
